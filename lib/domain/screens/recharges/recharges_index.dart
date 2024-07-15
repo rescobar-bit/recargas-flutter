346605cbf2789b6c00ceddbf16e645e818d71ebf
Rafael Escobar Gutierrez <rescobar@bit.lat>
2024-07-15 12:19:34 -0600

    Agregando barra estatica de sueldo

--- lib/domain/screens/recharges/recharges_index.dart
+++ lib/domain/screens/recharges/recharges_index.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:recargas_beta2/domain/screens/recharges/history_recharges.dart';
 import 'package:recargas_beta2/domain/screens/recharges/recharge.dart';
+import 'package:recargas_beta2/domain/widgets/balance_card.dart';
 
 class RechargesIndex extends StatelessWidget {
-  const RechargesIndex({super.key});
+  const RechargesIndex({
+    super.key
+  });
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: SingleChildScrollView(
-        padding: EdgeInsets.only(bottom: 20),
-        child: Column(
-          children: [
-            Recharge(),
-            HistoryRecharges()
-          ],
-        ),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            padding: EdgeInsets.only(bottom: 20),
+            child: Column(
+              children: [
+                Recharge(),
+                HistoryRecharges(),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: BalanceCard(
+                label: 'Saldo', 
+                balance: '\$100.00'
+              ),
+            )
+          )
+        ],
       )
     );
   }
---
+++ lib/domain/widgets/balance_card.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class BalanceCard extends StatelessWidget {
+  final String label;
+  final String balance;
+  const BalanceCard({
+    required this.label,
+    required this.balance,
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+      decoration: const BoxDecoration(
+        color: Color(0XFF141118),
+      ),
+      width: double.infinity,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            label,
+            style: const TextStyle(fontSize: 18),
+          ),
+          Row(
+            children: [
+              Text(
+                balance,
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(width: 6,),
+              IconButton(
+                onPressed: () {
+                },
+                icon: const Icon(Icons.refresh, color: Colors.blue,)
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
