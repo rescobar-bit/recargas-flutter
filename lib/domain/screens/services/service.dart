56f881f1f54a7398a9cf2db5cc80154938c6fb54
Rafael Escobar Gutierrez <rescobar@bit.lat>
2024-07-18 09:00:35 -0600

    Agrega función de calcular monto total a pagar

--- lib/domain/screens/services/service.dart
+++ lib/domain/screens/services/service.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:recargas_beta2/domain/widgets/card_select.dart';
+import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
 
-class Service extends StatelessWidget {
+class Service extends StatefulWidget {
   const Service({super.key});
+
+  @override
+  State<Service> createState() => _ServiceState();
+}
+
+class _ServiceState extends State<Service> {
+  TextEditingController codeReference = TextEditingController();
+  TextEditingController total = TextEditingController();
+
+  @override
+  void initState(){
+    total = TextEditingController(text: '10');
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +28,7 @@ class Service extends StatelessWidget {
       child: Column(
         children: [
           Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             color: Colors.grey.shade900,
             child: const CardSelect(
               hintText: 'Elige un servicio',
@@ -31,26 +50,97 @@ class Service extends StatelessWidget {
             ),
           ),
           Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             color: Colors.grey.shade900,
-            child: const CardSelect(
-              hintText: 'Elige un servicio',
-              label: 'Servicio',
-              items: [
-                DropdownMenuItem(
-                  value: 'cfe',
-                  child: Text('CFE'),
-                ),
-                DropdownMenuItem(
-                  value: 'xbox',
-                  child: Text('Xbox live'),
-                ),
-                DropdownMenuItem(
-                  value: 'totalplay',
-                  child: Text('Totalplay'),
+            child: FormBuilderTextField(
+              controller: codeReference,
+              style: const TextStyle(fontSize: 20),
+              name: 'reference',
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                labelText: 'Referencia',
+                labelStyle: const TextStyle(fontSize: 20),
+                suffixIcon: IconButton(
+                  onPressed: () async {
+                    String reference = await Navigator.push(
+                      context, 
+                      MaterialPageRoute(builder: (context) => const SimpleBarcodeScannerPage(
+                        appBarTitle: 'Escanea el QR',
+                        cancelButtonText: 'Cancelar',
+                        lineColor: "#0064FF",
+                        centerTitle: true,
+                      ),
+                      ));
+                      setState(()=> codeReference.text = (reference != '-1') ? reference : '');
+                  }, 
+                  icon: const Icon(Icons.qr_code_scanner),
+                  color: Colors.blue,
                 )
-              ],
-            ),
-          )
+              ),
+              validator: FormBuilderValidators.compose([
+                FormBuilderValidators.required()
+              ]),
+            )
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            color: Colors.grey.shade900,
+            child: FormBuilderTextField(
+              onChanged: (value) {
+                double newTotal = 0;
+                setState(() {
+                  newTotal = 10 + ((value!.isNotEmpty) ? double.parse(value) : 0);
+                  total.text = newTotal.toString();
+                });
+              },
+              style: const TextStyle(fontSize: 20),
+              name: 'totalAmount',
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                labelText: 'Monto a pagar',
+                labelStyle: TextStyle(fontSize: 20),
+              ),
+              validator: FormBuilderValidators.compose([
+                FormBuilderValidators.required()
+              ]),
+            )
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            color: Colors.grey.shade900,
+            child: FormBuilderTextField(
+              enabled: false,
+              initialValue: '10',
+              style: const TextStyle(fontSize: 20),
+              name: 'commission',
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                labelText: 'Monto a pagar',
+                labelStyle: TextStyle(fontSize: 20),
+              ),
+              validator: FormBuilderValidators.compose([
+                FormBuilderValidators.required()
+              ]),
+            )
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            color: Colors.grey.shade900,
+            child: FormBuilderTextField(
+              controller: total,
+              enabled: false,
+              style: const TextStyle(fontSize: 20),
+              name: 'commission',
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                labelText: 'Monto a pagar',
+                labelStyle: TextStyle(fontSize: 20),
+              ),
+              validator: FormBuilderValidators.compose([
+                FormBuilderValidators.required()
+              ]),
+            )
+          ),
             
         ],
       ),
