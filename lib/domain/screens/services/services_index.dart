05b8e03f51f799dc90e01eed323c575fcffb75b4
Rafael Escobar Gutierrez <rescobar@bit.lat>
2024-07-19 08:48:53 -0600

    Agrega el navbar de saldo

--- lib/domain/screens/services/services_index.dart
+++ lib/domain/screens/services/services_index.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:recargas_beta2/domain/screens/services/history_services.dart';
 import 'package:recargas_beta2/domain/screens/services/service.dart';
+import 'package:recargas_beta2/domain/widgets/balance_card.dart';
 
 class ServicesIndex extends StatelessWidget {
   const ServicesIndex({
@@ -21,7 +22,18 @@ class ServicesIndex extends StatelessWidget {
               ],
             ),
           ),
-
+          Positioned(
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: BalanceCard(
+                label: 'Saldo', 
+                balance: '\$500.00'
+              ),
+            )
+          )
         ],
       )
     );
