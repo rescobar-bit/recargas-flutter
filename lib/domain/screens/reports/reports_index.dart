289ddc27de6b2977cbd2beae00e0ff95267a5954
Rafael Escobar Gutierrez <rescobar@bit.lat>
2024-07-19 08:38:03 -0600

    Agrega el listado de recargas

--- lib/domain/screens/reports/reports_index.dart
+++ lib/domain/screens/reports/reports_index.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:recargas_beta2/data/recharges_data.dart';
+import 'package:recargas_beta2/domain/screens/recharges/recharge_card.dart';
+import 'package:recargas_beta2/domain/screens/recharges/recharge_details.dart';
+import 'package:recargas_beta2/domain/screens/recharges/text_icon_card.dart';
 import 'package:recargas_beta2/domain/widgets/bottom_sheet_filters.dart';
 
 class ReportsIndex extends StatefulWidget {
@@ -36,8 +40,44 @@ class _ReportsIndexState extends State <ReportsIndex> {
       body: SizedBox(
         child: Column(
           children: [
-            Text("Estatus: $currentStatus"),
-            Text("Periodo: $currentPeriod"),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: TextIconCard(
+                label: 'Últimas recargs',
+                icon: Icon(Icons.refresh_outlined),
+              ),
+            ),
+            Column(
+              children: rechargesData.map((recharge) {
+                return Column(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                            RechargeDetails(
+                              recharge: recharge,
+                            ),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: RechargeCard(
+                            company: recharge['company'] !,
+                            phoneNumber: recharge['phone_number'] !,
+                            date: recharge['date'] !,
+                            status: recharge['status'] !,
+                            amount: recharge['amount'] !,
+                          ),
+                      ),
+                    ),
+                  ],
+                );
+              }).toList(),
+            )
           ],
         ),
       ),
